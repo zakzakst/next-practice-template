c91980c937e4b4dfa7a08e9bb1fcd10407f697f2
zakzakst <e1433104@gmail.com>
2026-04-10 07:08:54 +0900

    zod, react hook formインストール

--- lib/msw/handlers/auth.ts
+++ lib/msw/handlers/auth.ts
@@ -3,7 +3,7 @@ import { users } from "@/dummy-db/user";
 import { API_MOCK_DEFAULT_DELAY } from "@/lib/api";
 import { HttpResponse, delay, http } from "msw";
 
-export const authMeHandler = http.get("*/auth/me", async (info) => {
+export const authMeHandler = http.get("*/auth/me", async (/* info */) => {
   // NOTE: MSWで認証のクッキーまわりの情報取得方法分からなかった
   // 単体テストなどの際に利用するのをメインにして、ログイン周りは総合テストとしてNext.js立ち上げている状態で行う。みたいな使い分けがいい？
   // console.log(info.request.headers);
