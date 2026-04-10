81071ca1cbccf5dd16d5dbb6a66dbe212ce9ba08
zakzakst <e1433104@gmail.com>
2026-04-11 03:20:13 +0900

    ユニットテスト作成

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [react()],
   test: {
-    include: ["tests/unit/**/*.spec.{ts,tsx}"],
+    include: [
+      "tests/unit/**/*.spec.{ts,tsx}",
+      // TODO: 上手く拡張機能が動かなかったので下記も足した（上の記述だけで大丈夫な認識なので、調べる）
+      "tests/unit/**/**/*.spec.{ts,tsx}",
+    ],
     setupFiles: ["vitest.setup.ts"],
     environment: "jsdom",
     globals: true,
